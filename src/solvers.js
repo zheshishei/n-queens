5ebaac3f1c9dfd9aea4cfa37758d4348ad8d4acb
zheshishei <asun0927@gmail.com>
2014-12-17 17:05:33 -0800

    temporary save

--- src/solvers.js
+++ src/solvers.js
@@ -14,8 +14,14 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
-
+  var solution = [];
+  var b = new Board({'n': n}); //fixme
+  var row;
+  for (var i = 0; i < n; i++) {
+    row = b.get(i);
+    row[i] = 1;
+    solution.push(row);
+  }
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
@@ -24,7 +30,12 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+
+  var solutionCount = 1;
+
+  for (var i = n; i > 0; i--) {
+    solutionCount *= i;
+  }
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
@@ -34,8 +45,45 @@ window.countNRooksSolutions = function(n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = [];
+  var b = new Board({'n':n});
+  var decisions = [];
+  var row;
+  // var available = new Array(n);
+  var lastDecision;
+  var maxIteration = 10000000, iter = 0;
 
+  while (solution.length < n && iter < maxIteration) {
+    iter++;
+
+    lastDecision = decisions[decisions.length - 1];
+
+    if (lastDecision === undefined) {
+      decisions.push([0,0]);
+      b.togglePiece(0,0);
+      continue;
+    }
+
+
+    // for (var i = 0; i < n; i++) {
+    //   if (!b.hasAnyQueenConflictsOn(lastDecision[0] + 1, i)) {
+
+    //   }
+    // }
+
+
+
+
+
+
+
+    b.togglePiece(lastDecision[0], lastDecision[1]);
+
+    }
+
+    // solution.push(b.get(i));
+
+  }
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
 };
